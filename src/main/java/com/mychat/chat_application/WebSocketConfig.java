b3f1f3fa1768f1f47aeae92f8cd15ea92f8c13a8
nisha <nishantprajapati999@gmail.com>
2022-07-31 21:50:44 +0530

    initial comment.

---
+++ src/main/java/com/mychat/chat_application/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.mychat.chat_application;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        System.out.println("in configure message broker");
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        System.out.println("in register stomp endpoints");
+        registry.addEndpoint("/our-websocket")
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
+    }
+}
---
+++ src/main/java/com/mychat/chat_application/dto/Message.java
@@ -0,0 +1,13 @@
+package com.mychat.chat_application.dto;
+
+public class Message {
+    private String messageContent;
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+}
